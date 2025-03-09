c7c259a4aeb59129fc4de31e59bcedca3a2a2b89
LincolnG4 <g97santos@gmail.com>
2025-03-09 22:59:57 +0100

    tests in repository

--- internal/controllers/pipelines.go
+++ internal/controllers/pipelines.go
@@ -1,9 +1,20 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/LincolnG4/Haku/internal/models"
 )
 
 func CreatePipeline(w http.ResponseWriter, r *http.Request) {
+	// Read the request body
+	newPipeline := models.Pipeline{}
+	if err := json.NewDecoder(r.Body).Decode(&newPipeline); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
 
+	fmt.Println(newPipeline)
 }
--- internal/models/pipelines.go
+++ internal/models/pipelines.go
@@ -2,21 +2,22 @@ package models
 
 import "time"
 
-type Pipelines struct {
+type Pipeline struct {
 	ID          string         `json:"id"`
 	Name        string         `json:"name"`
-	Description string         `json:"description"`
-	Tasks       []Task         `json:"tasks"`
-	Schedule    string         `json:"schedule"` // Cron expression for time-based triggers
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	Metadata    map[string]any `json:"metadata"`
+	Description string         `json:"description,omitempty"`
+	Tasks       []Task         `json:"tasks,omitempty"`
+	Schedule    string         `json:"schedule,omitempty"` // Cron expression for time-based triggers
+	CreatedAt   time.Time      `json:"created_at,omitempty"`
+	UpdatedAt   time.Time      `json:"updated_at,omitempty"`
+	Metadata    map[string]any `json:"metadat,omitempty"`
+	Status      string         `json:"status,omitempty"`
 }
 
 type Task struct {
 	ID           string       `json:"id"`
 	Name         string       `json:"name"`
-	Execute      func() error `json:"execute"`
-	Dependencies []string     `json:"dependencies"`
+	Execute      func() error `json:"execute,omitempty"`
+	Dependencies []string     `json:"dependencies,omitempty"`
 	Status       string       `json:"status"`
 }
